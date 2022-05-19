ca362743965140d0829badb59fe785d947bc2cbd
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-05-19 14:06:01 -0600

    cleaning up

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -4,7 +4,6 @@ import LikeSection from './LikeSection';
 import PostHeader from './PostHeader';
 
 const Post = props => {
-  // 🔥 Make sure the parent of Post is passing the right props!
   const { post, likePost } = props;
 
   return (
@@ -13,9 +12,7 @@ const Post = props => {
       <div className="post-image-wrapper">
         <img alt="post thumbnail" className="post-image" src={post.imageUrl} />
       </div>
-      {/* Is LikeSection getting all the props it needs to work correctly? */}
       <LikeSection likePost={() => likePost(post.id)} numberOfLikes={post.likes} />
-      {/* Comments also wants its props! */}
       <Comments comments={post.comments}/>
     </div>
   );
